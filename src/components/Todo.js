4d108fc8efae2aab4524e4e4944adf11bdd00b03
shubh <shubhamdixit863@gmail.com>
2024-03-15 22:07:10 +0530

    redux todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,13 @@ import React,{useState} from 'react'
 import { Input,Button } from 'antd';
 import { Col, Row } from 'antd';
 import { addtodo,changeInputState } from '../slices/todoslice';
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
+import { Divider, List, Typography } from 'antd';
 
 
 const Todo = () => {
+    const todoState=useSelector(state=>state.todoReducer)
+    console.log(todoState)
     const dispatch=useDispatch();
     const [state,setState]=useState();
 
@@ -27,7 +30,27 @@ const Todo = () => {
 
       <Input placeholder="Todo" onChange={handleChange} />;
       <Button type="primary" style={{marginTop:"20px"}} onClick={handleClick}>Add Todo</Button>
+
+
+
+      <Divider orientation="left">Default Size</Divider>
+    <List
+      header={<div>Header</div>}
+      footer={<div>Footer</div>}
+      bordered
+      dataSource={todoState.list}
+      renderItem={(item) => (
+        <List.Item>
+          <Typography.Text mark>[ITEM]</Typography.Text> {item}
+        </List.Item>
+      )}
+    />
+
+
       </Col>
+
+
+
      
     </Row>
      
